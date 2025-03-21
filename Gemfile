0b09e8cb8f1c8228892c264dfdc8ffbfe667b4d8
Nipuna Rambukkanage <nipunaram@gmail.com>
2025-03-22 04:57:43 +0530

    Added tailwind

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem "jbuilder"
 gem "devise"
 
 gem "kaminari"
+
+gem "tailwindcss-rails", "~> 2.0"
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def status_color(status)
+    case status.downcase
+    when "completed" then "bg-green-100 text-green-800"
+    when "in progress" then "bg-blue-100 text-blue-800"
+    when "not started" then "bg-gray-100 text-gray-800"
+    else "bg-indigo-100 text-indigo-800"
+    end
+  end
 end
